36f69bf2ebe419d4823e3eda2ca0b32534b0a012
wangonya <kwangonya@gmail.com>
2019-08-08 12:24:12 +0300

    add auth urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,10 +5,7 @@ from recipes.recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/token/', jwt_views.TokenObtainPairView.as_view(),
-    #      name='token_obtain_pair'),
-    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
-    #      name='token_refresh'),
+    path('accounts/', include('allauth.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('recipes/', views.RecipesView.as_view(), name='recipes')
