593e38ec04e35708d55b3b7171aa433efb0e722b
wangonya <kwangonya@gmail.com>
2019-08-08 19:36:09 +0300

    add signup tests

--- recipes/authentication/tests/test_signup.py
+++ recipes/authentication/tests/test_signup.py
@@ -1,6 +1,19 @@
-def test_signup(base_test_case):
-    response = base_test_case.client.post('/rest-auth/registration/',
-                                          base_test_case.test_user,
-                                          format='json')
-    print(response.data)
-    assert response.status_code == 200
+import pytest
+
+url = '/rest-auth/registration/'
+test_user = {
+        'username': 'test',
+        'email': 'test@mail.com',
+        'password1': 'testPass1!',
+        'password2': 'testPass1!'
+    }
+
+
+@pytest.mark.parametrize('url, data', [(url, test_user)])
+def test_signup_success(post_data):
+    assert post_data.status_code == 201
+
+
+@pytest.mark.parametrize('url, data', [(url, {})])
+def test_signup_failure(post_data):
+    assert post_data.status_code == 400
--- recipes/conftest.py
+++ recipes/conftest.py
@@ -1,23 +1,27 @@
 import django
 import pytest
 
-from django.test import TestCase
 from rest_framework.test import APITestCase, APIClient
 
 django.setup()
 
 
-class BaseTestCase(TestCase):
+class BaseTestCase(APITestCase):
     client = APIClient()
 
-    test_user = {
-        'username': 'test',
-        'email': 'test@mail.com',
-        'password1': 'testPass1!',
-        'password2': 'testPass1!'
-    }
+
+@pytest.fixture(autouse=True)
+def enable_db_access_for_all_tests(db):
+    """enable db access for all tests"""
+    pass
 
 
 @pytest.fixture
 def base_test_case():
     yield BaseTestCase
+
+
+@pytest.fixture
+def post_data(base_test_case, url, data):
+    """helper function to test posting data to the specified url """
+    return base_test_case.client.post(url, data, format='json')
