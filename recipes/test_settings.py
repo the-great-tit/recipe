10dfc41e2596523f441820bb4701b7e485f9785e
wangonya <kwangonya@gmail.com>
2019-08-13 13:42:23 +0300

    add .travis file

--- recipes/test_settings.py
+++ recipes/test_settings.py
@@ -1,10 +1,3 @@
 from .settings import *             # NOQA
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:'
-    }
-}
-
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
